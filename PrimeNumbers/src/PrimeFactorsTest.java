fcd81c77bda39abeeb61a8d15a1e1f50dce97fbe
Michael Frank <frankmp@rose-hulman.edu>
2012-03-27 21:17:01 -0400

    Created testSIx

--- PrimeNumbers/src/PrimeFactorsTest.java
+++ PrimeNumbers/src/PrimeFactorsTest.java
@@ -26,6 +26,11 @@ public class PrimeFactorsTest {
 	public void testFour() {
 		assertEquals(list(2, 2), PrimeNumbers.generatePrimes(4));
 	}
+	
+	@Test
+	public void testSix() {
+		assertEquals(list(2, 3), PrimeNumbers.generatePrimes(6));
+	}
 
 	private ArrayList<Integer> list(int... ints) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
